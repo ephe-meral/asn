c4bf180f1b0221d2157cdf12d6b901c2c1d12556
ephe-meral <j.appel.online@gmail.com>
2016-07-05 12:25:52 +0200

    cleanup v.0.1.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ASN.Mixfile do
 
   def project do
     [app: :asn,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
